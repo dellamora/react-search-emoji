f3e4935b85b81d6074c276269167edf00b380eb6
Francielle Dellamora Menegotto <francidellamora@gmail.com>
2021-10-21 22:59:27 -0300

    added emoji filter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import EmojiArr from "./emoji.json";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const newData = EmojiArr.filter((emoji) =>
+      emoji.title.toLowerCase().includes(search.toLowerCase())
+    );
+    setData(newData);
+  }, [search]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Emoji Search</h1>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {data.map((emoji) => (
+        <div key={emoji.title}>
+          <div
+            onClick={() => {
+              navigator.clipboard.writeTex(emoji.symbol);
+            }}
+          >
+            {emoji.symbol} {emoji.title}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
